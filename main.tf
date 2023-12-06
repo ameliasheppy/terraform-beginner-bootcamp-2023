8c59cf7b92020fc332f209a9ad57854337979596
Amy Stanley <thegirl033007@gmail.com>
2023-12-06 18:41:26 +0000

    #40 finished implementation of tf provider

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -42,3 +42,16 @@ provider "terratowns" {
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 # }
+
+resource "terratowns_home" "home" {
+  name = "Why the Outer Banks of North Carolina are a great vacation spot"
+  description = <<-DESCRIPTION
+    The Outer Banks is a long strip of barrier islands with lots of peaceful areas 
+    and fun activities suitable for all ages and activity levels. Come enjoy some fun year-round!
+  DESCRIPTION
+
+  # domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "the-nomad-pad"
+  content_version = 1
+}
